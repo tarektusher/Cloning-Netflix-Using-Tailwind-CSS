4da870c11f054080d70325b8da31f3a10046573b
Md Tarek Hossain Tusher <mdtarek168504@gmail.com>
2023-07-12 00:06:05 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors : {
+        "dark" : "#000614",
+        "dark2" : "#C00000",
+        "text" : "white",
+        "primary" : "#E50914",
+        "secondary" : "red",
+        "form_color_hover" :"#4E4E4E",
+        "form_color" :"#292926",
+        "footer" :"#B3ADA8",
+        "gray" :"#757575",
+      },
+    },
+  },
+  plugins: [],
+}
+
